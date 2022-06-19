a8ef4b8e0147639fa2ffd29d59b04b54f41e8ce8
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-18 22:18:17 -0700

    finishedgit statusgit statusgit status!

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-
+let count = 0;
 let randomNumber = Math.floor(Math.random() * 100)
-let count = 0
+
+// Couldn't get this to work
+function HelperFunction(guess){
+    if (guess > 100){
+        guess = 100
+    }
+}
 
 function PlayGame(event){
     event.preventDefault()
@@ -9,10 +15,33 @@ function PlayGame(event){
     let incorrect = document.getElementById("incorrect")
     let guessNotifier = document.getElementById("guessCount")
     let guessCount = document.getElementById("guessHolder")
+    let input = document.getElementById("input")
+
+    if (guess > 100){
+        guess = 100
+    }
 
     if (guess < randomNumber){
         if (count == 0){
-            incorrect.innerText == "Guess HIGHER!"
+            guessNotifier.innerText = "Current Guesses:";
         }
+        incorrect.innerText = "Guess HIGHER!";
+        guessNotifier.innerText += `\n${guess}: Too low!`
     }
+    else if (guess > randomNumber){
+        if (count == 0){
+            guessNotifier.innerText = "Current Guesses:";
+        }
+        incorrect.innerText = "Guess LOWER!";
+        guessNotifier.innerText += `\n${guess}: Too high!`
+    }
+    else{
+        if (count == 0){
+            guessNotifier.innerText = "Current Guesses:";
+        }
+        incorrect.innerText = "That's correct! Nice work!";
+        guessNotifier.innerText += `\n${guess}: Spot on!`
+    }
+    count++;
+    input.value = "";
 }
