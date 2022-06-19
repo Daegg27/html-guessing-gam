c8b76336f119cbaaf4c42bd65bd40825609e5d68
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-18 20:02:32 -0700

    almost there

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// Your function(s) should go here that will interact with the webpage or DOM
+
+let randomNumber = Math.floor(Math.random() * 100)
+let count = 0
+
+function PlayGame(event){
+    event.preventDefault()
+
+    let guess = Number(event.target.elements[0].value);
+    let incorrect = document.getElementById("incorrect")
+    let guessNotifier = document.getElementById("guessCount")
+    let guessCount = document.getElementById("guessHolder")
+
+    if (guess < randomNumber){
+        if (count == 0){
+            incorrect.innerText == "Guess HIGHER!"
+        }
+    }
+}
